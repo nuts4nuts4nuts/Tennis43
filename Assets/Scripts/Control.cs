f0a4aed8eb0e545a1017ae40f249211d71d8f871
nuts4nuts4nuts <nuts4nuts4nuts@gmail.com>
2015-02-23 02:37:17 -0500

    did a bunch of stuff.

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Control : MonoBehaviour
+{
+    //Movement
+    public float maxSpeed = 10.0f;
+    public AnimationCurve movementCurve = AnimationCurve.Linear(0, 0, 1, 1);
+
+    //Visuals
+    public Image associatedTri;
+    private float boostJuice = 1.0f;
+    private float boostJuiceBonus = 0.3f;
+
+    //Hitting
+    public float baseCharge = 1.0f;
+    private float currentCharge = 1.0f;
+    private SphereCollider hitRange;
+
+    [HideInInspector]
+    public List<BallScript> currentBalls;
+
+    void Awake()
+    {
+        hitRange = (SphereCollider)GetComponent(typeof(SphereCollider));
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        boostJuice += boostJuiceBonus;
+
+        float movementX = Input.GetAxis("Horizontal");
+        float movementZ = Input.GetAxis("Vertical");
+        float triggerJuice = -Input.GetAxis("Trigger");
+
+        triggerJuice += 1.2f; //Buffer
+
+        float modulatedTriggerJuice = (triggerJuice + 8.75f) * 0.03f;
+
+        if (boostJuice < modulatedTriggerJuice)
+        {
+            triggerJuice = 1;
+            boostJuice -= modulatedTriggerJuice;
+        }
+        else
+        {
+            boostJuice -= modulatedTriggerJuice;
+        }
+
+        boostJuice = Mathf.Clamp(boostJuice, 0, 1.0f);
+        associatedTri.fillAmount = boostJuice;
+
+        float curvedMovementX = movementCurve.Evaluate(movementX);
+        float curvedMovementZ = movementCurve.Evaluate(movementZ);
+
+        Vector3 newVelocity = rigidbody.velocity;
+        newVelocity.x = curvedMovementX * maxSpeed * (triggerJuice * 3) * Time.deltaTime;
+        newVelocity.z = curvedMovementZ * maxSpeed * (triggerJuice * 3) * Time.deltaTime;
+        rigidbody.velocity = newVelocity;
+
+        { //Hitting the ball
+            if (Input.GetButton("Hit"))
+            {
+                currentCharge += 0.5f * Time.deltaTime;
+            }
+
+            if (Input.GetButtonUp("Hit"))
+            {
+                //Hit the ball
+                if(currentBalls.Count > 0)
+                {
+                    BallScript currentBall = currentBalls[0];
+                    GameObject ball = currentBall.gameObject;
+                    ball.transform.forward = gameObject.transform.forward;
+
+                    Vector3 newVelo = rigidbody.velocity;
+
+                    newVelo.z += currentCharge * 200 * Time.deltaTime;
+                    newVelo.y += currentCharge * 100 * Time.deltaTime;
+
+                    ball.rigidbody.velocity = newVelo;
+                }
+
+                currentCharge = baseCharge;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/HitRange.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitRange : MonoBehaviour
+{
+    private Control player;
+
+    void Awake()
+    {
+        player = (Control)GetComponentInParent(typeof(Control));
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        BallScript bs = (BallScript)other.GetComponent(typeof(BallScript));
+        
+        if(bs) //lol
+        {
+            player.currentBalls.Add(bs);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        BallScript bs = (BallScript)other.GetComponent(typeof(BallScript));
+
+        if(bs)
+        {
+            player.currentBalls.Remove(bs);
+        }
+    }
+}
